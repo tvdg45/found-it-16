53d57a1a35238940f96601968675d4742b0c9f42
Tim van der Graaff <timvdg45@gmail.com>
2022-04-03 21:23:14 -0500

    Create Directory_Hider.java

---
+++ src/main/java/Directory_Hider.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/")
+public class Directory_Hider {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    String home() {
+		
+        return "";
+    }
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Directory_Hider.class, args);
+    }
+}
